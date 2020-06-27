b301d9cf189297a92365cb21a74f917366d102d3
Sridha Maram <sridhar988417@gmail.com>
2020-06-27 17:27:21 +0530

    intial setup

---
+++ src/main/java/org/ssol/reactinspringboot/ReactInSpringBootApplication.java
@@ -0,0 +1,13 @@
+package org.ssol.reactinspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactInSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactInSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/ssol/reactinspringboot/ReactInSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ssol.reactinspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactInSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
